57579ac1d106b1cad28e2b5c4c25923d8a9c8235
JINSUNG <lucidstar42@gmail.com>
2019-11-02 20:00:29 +0900

    Safe Args 사용

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -22,8 +22,9 @@ class TitleFragment : Fragment() {
                 inflater, R.layout.fragment_title, container, false
         )
         binding.playButton.setOnClickListener {view: View ->
-            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+//            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
 //            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+            view.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
             setHasOptionsMenu(true)
         }
 
